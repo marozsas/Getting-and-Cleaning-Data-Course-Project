81687e6711c7165d109ad1e8f2dbc99c21576140
Miguel Rozsas <miguel@rozsas.eng.br>
2016-12-03 16:39:16 -0200

    improved code by using summarise_all

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,8 @@
-
 #####
 #1 Merges the training and the test sets to create one data set.
 #####
 library (dplyr)
-#setwd ("/home/miguel/src/datasciencecoursera/CleaningData/assignment")
+setwd ("/home/miguel/src/datasciencecoursera/CleaningData/assignment")
 
 #You should create one R script called run_analysis.R that does the following.
 #1 Merges the training and the test sets to create one data set.
@@ -16,26 +15,26 @@ fileActivityLabels <-  "./UCI HAR Dataset/activity_labels.txt"
 act_lbl <- read.table(fileActivityLabels, stringsAsFactors = TRUE)
 
 #4 Appropriately labels the data set with descriptive variable names.
-# read Train data and labels:
+# read Train data 
 fileData <- "./UCI HAR Dataset/train/X_train.txt"
 train <- read.table(fileData, row.names=NULL)
-
+# read labels
 fileDataLabels <- "./UCI HAR Dataset/features.txt"
 labels <- read.table(fileDataLabels, row.names=NULL)
-
-# change the names of columns
+# change the names of columns. Use make.names to avoid duplicated names.
 names(train) <- make.names(labels$V2, unique = TRUE)
 
+# read subject table
 fileSubject <- "./UCI HAR Dataset/train/subject_train.txt"
 subject <- read.table(fileSubject, row.names=NULL)
-
+# read activity table
 fileActivity <-  "./UCI HAR Dataset/train/y_train.txt"
 activity <- read.table(fileActivity, row.names=NULL)
 
-# add subject
+# add subject to the train set
 train$subject <- subject$V1
 
-# add activity 
+# add activity to the train set
 train$activity <- factor(activity$V1, levels = act_lbl$V1, labels=act_lbl$V2)
 
 # re-order 
@@ -44,20 +43,19 @@ train <- train %>% select (subject, activity , everything())
 # read Test data:
 fileData <- "./UCI HAR Dataset/test/X_test.txt"
 test <- read.table(fileData, row.names=NULL)
-
-# change the names of columns
+# change the names of columns. Use make.names to avoid duplicated names.
 names(test) <- make.names(labels$V2, unique = TRUE)
-
+# read subject table
 fileSubject <- "./UCI HAR Dataset/test/subject_test.txt"
 subject <- read.table(fileSubject)
-
+# read activity table
 fileActivity <- "./UCI HAR Dataset/test/y_test.txt"
 activity <- read.table(fileActivity)
 
-# add subject
+# add subject to the train set
 test$subject <- subject$V1
 
-# add activity 
+# add activity to the train set
 test$activity <- factor(activity$V1, levels = act_lbl$V1, labels=act_lbl$V2)
 
 # re-order 
@@ -77,7 +75,7 @@ data <- rbind (train, test)
 scol <- colnames(data)
 # only column names that have mean or std
 scol <- scol[grep ("mean|std", scol, ignore.case=TRUE)]
-# a sub set
+# build a sub set
 data2 <- data[, c("subject", "activity", scol)]
 #####
 #End of part 2
@@ -90,12 +88,16 @@ data2 <- data[, c("subject", "activity", scol)]
 #####
 #5 From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 #####
-# get only the measuments data
-data2.x <- subset(data2, select= -c(subject, activity))
+# get only the measuments data - SOLUTION 1
+#data2.x <- subset(data2, select= -c(subject, activity))
 # for each activity and each subject
-data2.group <- subset(data2, select= c(activity, subject))
+#data2.group <- subset(data2, select= c(activity, subject))
 # data set with the average of each variable
-data5 <- aggregate(data2.x, data2.group, mean)
+#data5 <- aggregate(data2.x, data2.group, mean)
+#
+# get only the measuments data - SOLUTION 2 - best
+data5 <- data2 %>% group_by(subject, activity) %>% summarise_all("mean")
+
 fileOutput= "dataAverage.txt"
 write.table(data5, fileOutput, row.names=FALSE)
 #####
