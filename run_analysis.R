53ff6ed8b84197c3de120373cf0e082548020ac8
Miguel Rozsas <miguel@rozsas.eng.br>
2016-12-03 08:32:19 -0200

    removed partition from code; It is not necessary.

--- run_analysis.R
+++ run_analysis.R
@@ -9,16 +9,7 @@ library (dplyr)
 #1 Merges the training and the test sets to create one data set.
 # from David Hood infamous post, "Do we need the inertial folder ?  Short answer- no."
 
-
-# The resultant file has the the subject, partition info (train or test), , the activity, and the original time/freq data; total of 10299 lines and 563 columns.
-
 #3 Uses descriptive activity names to name the activities in the data set
-# by my own I will use descriptive names for the name of original data set too
-
-# train/test factor to identify the origin of data set
-partition <- factor (c("train", "test"))
-p_train <- partition[1]
-p_test  <- partition[2]
 
 # activity as factor
 fileActivityLabels <-  "./UCI HAR Dataset/activity_labels.txt"
@@ -32,13 +23,8 @@ train <- read.table(fileData, row.names=NULL)
 fileDataLabels <- "./UCI HAR Dataset/features.txt"
 labels <- read.table(fileDataLabels, row.names=NULL)
 
-# normalize labels by removing parenthesis and commas
-labels$norm <- gsub ("()", "", labels[[2]], fixed= TRUE)
-labels$norm <- gsub ("(", "_", labels$norm, fixed=TRUE)
-labels$norm <- gsub (")", "", labels$norm, fixed=TRUE)
-labels$norm <- gsub (",", "_", labels$norm, fixed=TRUE)
 # change the names of columns
-names(train) <- make.names(labels$norm, unique = TRUE)
+names(train) <- make.names(labels$V2, unique = TRUE)
 
 fileSubject <- "./UCI HAR Dataset/train/subject_train.txt"
 subject <- read.table(fileSubject, row.names=NULL)
@@ -46,24 +32,21 @@ subject <- read.table(fileSubject, row.names=NULL)
 fileActivity <-  "./UCI HAR Dataset/train/y_train.txt"
 activity <- read.table(fileActivity, row.names=NULL)
 
-# merge subject, partition TRAIN, activity
+# add subject
 train$subject <- subject$V1
-train$partition <- rep(p_train, nrow(train)) #train
-# activity as numeric
-#train$activity <- activity$V1
-# activity as factor/descriptive name
+
+# add activity 
 train$activity <- factor(activity$V1, levels = act_lbl$V1, labels=act_lbl$V2)
 
 # re-order 
-train <- train %>% select (subject, partition, activity , everything())
-
+train <- train %>% select (subject, activity , everything())
 
 # read Test data:
 fileData <- "./UCI HAR Dataset/test/X_test.txt"
 test <- read.table(fileData, row.names=NULL)
 
 # change the names of columns
-names(test) <- make.names(labels$norm, unique = TRUE)
+names(test) <- make.names(labels$V2, unique = TRUE)
 
 fileSubject <- "./UCI HAR Dataset/test/subject_test.txt"
 subject <- read.table(fileSubject)
@@ -71,21 +54,17 @@ subject <- read.table(fileSubject)
 fileActivity <- "./UCI HAR Dataset/test/y_test.txt"
 activity <- read.table(fileActivity)
 
-# merge subject, partition TEST, activity
+# add subject
 test$subject <- subject$V1
-test$partition <- rep(p_test, nrow(test)) #train
-# activity as numeric
-# test$activity <- activity$V1
-# activity as factor/descriptive name
+
+# add activity 
 test$activity <- factor(activity$V1, levels = act_lbl$V1, labels=act_lbl$V2)
 
 # re-order 
-test <- test %>% select (subject, partition, activity, everything())
+test <- test %>% select (subject,  activity, everything())
 
 # bind train and test sets
 data <- rbind (train, test)
-fileOutput= "dataMerged.txt"
-write.table(data, fileOutput, row.names=FALSE)
 #####
 # End of part 1
 #####
@@ -99,9 +78,7 @@ scol <- colnames(data)
 # only column names that have mean or std
 scol <- scol[grep ("mean|std", scol, ignore.case=TRUE)]
 # a sub set
-data2 <- data[, c("subject", "partition", "activity", scol)]
-fileOutput= "dataMeanStd.txt"
-write.table(data2, fileOutput, row.names=FALSE)
+data2 <- data[, c("subject", "activity", scol)]
 #####
 #End of part 2
 #####
@@ -114,11 +91,11 @@ write.table(data2, fileOutput, row.names=FALSE)
 #5 From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 #####
 # get only the measuments data
-data.x <- subset(data, select= -c(subject, activity, partition))
+data2.x <- subset(data2, select= -c(subject, activity))
 # for each activity and each subject
-data.group <- subset(data, select= c(activity, subject))
+data2.group <- subset(data2, select= c(activity, subject))
 # data set with the average of each variable
-data5 <- aggregate(data.x, data.group, mean)
+data5 <- aggregate(data2.x, data2.group, mean)
 fileOutput= "dataAverage.txt"
 write.table(data5, fileOutput, row.names=FALSE)
 #####
